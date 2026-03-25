6777dab7f78458ecc3d04f7ebaf5524e93bf1c2d
Khaled <Khaled@DESKTOP-30UASTD>
2026-03-25 15:34:32 +0200

    Add Categories Model

---
+++ Bookify.Web/Core/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookify.Web.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "الاسم لا يتعدي 50 حرف ")]
+        public string Name { get; set; } = null!;
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime? LastUpdatedOn { get; set; }
+    }
+}
--- Bookify.Web/Data/ApplicationDbContext.cs
+++ Bookify.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Bookify.Web.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Web.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<Category>().Property(e => e.CreatedOn).HasDefaultValueSql("GETDATE()");
+
+            base.OnModelCreating(builder);
+        }
     }
 }
